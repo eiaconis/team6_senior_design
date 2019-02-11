69a2408192e8cde5bcf32f4c198ec3b11feed768
OIDUser <oiduser@OIDUsers-MacBook-Air.local>
2019-02-11 17:32:39 -0500

    Create user updates in DB

--- senior_design_2019/senior_design_2019/Controllers/CreateAccountViewController.swift
+++ senior_design_2019/senior_design_2019/Controllers/CreateAccountViewController.swift
@@ -10,7 +10,14 @@ import UIKit
 
 class CreateAccountViewController: UIViewController {
     
+    // Database Instance
+    let database: DatabaseAccess = DatabaseAccess.getInstance()
+    
     @IBOutlet weak var continueButton: UIButton!
+    
+    // Text Fields
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var firstNameTextField: UITextField!
     @IBOutlet weak var lastNameTextField: UITextField!
     @IBOutlet weak var phoneNumberTextField: UITextField!
@@ -25,7 +32,32 @@ class CreateAccountViewController: UIViewController {
     }
     
     @IBAction func continueButtonPressed(_ sender: Any) {
-        print("continueButton pressed")
+        let email = emailField.text ?? ""
+        let password = passwordField.text ?? ""
+        let firstName = firstNameTextField.text ?? ""
+        let lastName = lastNameTextField.text ?? ""
+        let phone = phoneNumberTextField.text ?? ""
+        if email == "" {
+            createAlert(title: "Email required")
+        } else if password == "" {
+            createAlert(title: "Password required")
+        } else if firstName == "" {
+            createAlert(title: "First name required")
+        } else if lastName == "" {
+            createAlert(title: "Last name required")
+        } else if phone == "" {
+            createAlert(title: "Phone number required")
+        } else {
+            let newUser = User(email: email, firstName: firstName, lastName: lastName, phoneNumber: phone)
+            database.createAccount(newUser: newUser, goal: nil, password: password, view: self)
+        }
+    }
+    
+    func createAlert(title: String) {
+        let alert = UIAlertController(title: title, message: "", preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "Try Again", style: UIAlertAction.Style.default, handler: {(action) in alert.dismiss(animated: true, completion: nil)}))
+        
+        self.present(alert, animated: true, completion: nil)
     }
     
 }
