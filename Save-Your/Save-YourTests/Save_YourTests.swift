f416abf0dfdc0f71a531339cdf927e5513854b64
OIDUser <oiduser@OIDUsers-MacBook-Air.local>
2019-03-28 02:00:12 -0400

    Duplicate project with new name

---
+++ Save-Your/Save-YourTests/Save_YourTests.swift
@@ -0,0 +1,33 @@
+//
+//  Save_YourTests.swift
+//  Save-YourTests
+//
+//  Created by OIDUser on 3/28/19.
+//
+
+import XCTest
+@testable import Save_Your
+
+class Save_YourTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
